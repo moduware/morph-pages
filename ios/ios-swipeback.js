9645222ed12b190f491c53603fb2bbc65e15dff7
Clint <clintp1@gmail.com>
2018-01-29 14:28:15 +1100

    move _trackMove() and _trackEnd to ios-swipeback.js file

--- ios/ios-swipeback.js
+++ ios/ios-swipeback.js
@@ -13,9 +13,9 @@ function _onTrack(self, event) {
     self._swipeStarted = true;
     this._trackStart(self, track);
   } else if (track.state == 'track' && self._swipeStarted) {
-    self._trackMove(track);
+    this._trackMove(self, track);
   } else if (track.state == 'end' && self._swipeStarted) {
-    self._trackEnd(track);
+    this._trackEnd(self, track);
   }
 }
 
@@ -38,4 +38,48 @@ function _trackStart(self, trackData) {
 
   // Prevent regular touchmove event (disables vertical scroll) the other half which removes this is in the _trackEnd()
   //TODO: window.addEventListener('touchmove', this._preventTouchMove);
+}
+
+/** 
+ *  Gets all data event.details as long as track.state = 'track'. 
+ */
+function _trackMove(self, trackData) {
+  //TODO: send data for animation progression
+  self._animatePages(trackData.dx);
+}
+
+/**
+ * This computes transition if pages to swipe or move back to current page. 
+ */
+function _trackEnd(self, trackData) {
+  if (self._swipeStarted == false) {
+    return;
+  }
+
+  // set up current and previous pages and set listener for 'transitionend'
+  self._currentPageElement.style.transition = self._computeTransition(1);
+  self._currentPageElement.addEventListener('transitionend', self._pageTransitionEndHandler);
+  self._previousPageElement.style.transition = self._computeTransition(1);
+  self._previousPageElement.addEventListener('transitionend', self._pageTransitionEndHandler);
+
+  // The element is swipe away when swiping get passed the threshold
+  self._completeSwipe = Math.abs(trackData.dx) > self._getOffsetWidth() * self.threshold;
+  if (self._completeSwipe) {
+    // update selected
+    self._selectPage(self._leftCandidate);
+    // trigger the animation in the right direction
+    self._animatePages(self._getOffsetWidth());
+
+    // update userSelected property so selectNext and selectPrevious works
+    self.userSelected = self._valueToIndex(self.selected);
+    // remove the last element of navigationHistory when swipe is complete
+
+    self.navigationHistory.pop();
+  } else {
+    // swipe not long enough so go back to current page
+    self._animatePages(0);
+  }
+
+  // enable regular touchmove event ( enables vertical scroll again)
+  // window.removeEventListener('touchmove', this._preventTouchMove);
 }
