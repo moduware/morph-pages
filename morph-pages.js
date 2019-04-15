a388c40fbc2b953ea3319f144bf7c589dad3e8fe
Alex Chernov <alexusblack@gmail.com>
2019-04-15 16:28:10 +1000

    going for animations

--- morph-pages.js
+++ morph-pages.js
@@ -15,20 +15,27 @@ class MorphPages extends LitElement {
     return [
       css`
 
-        :host {
-        }
+      :host {
+        display: block;
+        position: relative;
+      }
+
+      ::slotted(.page) {
+        position: absolute;
+        top: 0; left: 0; bottom: 0; right: 0;
+      }
 
       ::slotted(:not(.page--current)) {
         display: none;
       }
 
-        /* if iOS assigning colors from iOS colors table */
-        :host([platform="ios"]) {
-        }
+      /* if iOS assigning colors from iOS colors table */
+      :host([platform="ios"]) {
+      }
 
-        /* if Android assigning colors from Android colors table */
-        :host([platform="android"]) {
-        }
+      /* if Android assigning colors from Android colors table */
+      :host([platform="android"]) {
+      }
 
       `
     ];
@@ -78,14 +85,20 @@ class MorphPages extends LitElement {
     // }
     if(changedProperties.has('current-page')) {
       const currentPageName = this['current-page'];
-      const pages = this.getElementsByClassName('page');
-      for(let page of pages) {
-        if(page.getAttribute('name') == currentPageName) {
-          page.classList.add('page--current');
-        } else {
-          page.classList.remove('page--current');
-        }
+      let newPage = this.querySelector(`[name=${currentPageName}]`);
+      let oldPage = null;
+      if(typeof changedProperties['current-page'] != 'undefined') {
+        oldPage = this.querySelector(`[name=${changedProperties['current-page']}]`);
       }
+      // for(let page of pages) {
+      //   if(page.getAttribute('name') == currentPageName) {
+      //     newPage = page;
+      //     //page.classList.add('page--current');
+      //   } else {
+      //
+      //     page.classList.remove('page--current');
+      //   }
+      // }
     }
   }
 
